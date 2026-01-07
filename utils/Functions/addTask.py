639b360644ae58a887157030eb2b68eb0f2f2237
Hydra <hydraplays0001@gmail.com>
2026-01-07 21:14:07 +0530

    Added addTask Function 'Note': (Not Bug Proof Still Working!)

--- utils/Functions/addTask.py
+++ utils/Functions/addTask.py
@@ -1,1 +1,14 @@
 from time import sleep
+from utils.shared.shared import tasks
+
+
+def addTask():
+    Task: str = input("Enter Your Task: ")
+    sleep(2)
+    dictionary: dict[str, bool] = {Task: False}
+    tasks.update(dictionary)
+    print("Added Task Successfully! ")
+
+
+if __name__ == "__main__":
+    addTask()

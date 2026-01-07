a6a0dd1fa82c0e7546c6cc96b4e435f14cb1d89b
Hydra <hydraplays0001@gmail.com>
2026-01-07 21:23:23 +0530

    Solving a bug

--- utils/Functions/choice.py
+++ utils/Functions/choice.py
@@ -1,22 +1,25 @@
 from utils.Functions.viewTask import viewTasks
+from utils.Functions.addTask import addTask
+from time import sleep
 
 
 def userChoice():
-    while True:
-        print(f"{'\t' * 8}***-***-TODO LIST APP-***-***{'\t' * 2}")
-        print("Press:")
-        print("'1' To View The Tasks.")
-        print("'2' To Add a Task.")
-        print("'3' To Delete a Task.")
-        print("'4' To Update The Tasks.")
-        print("'5' To Exit.")
+    print(f"{'\t' * 8}***-***-TODO LIST APP-***-***{'\t' * 2}")
+    print("Press:")
+    print("'1' To View The Tasks.")
+    print("'2' To Add a Task.")
+    print("'3' To Delete a Task.")
+    print("'4' To Update The Tasks.")
+    print("'5' To Exit.")
 
+    while True:
         choice = input("Enter your choice: ")
 
         if choice == "1":
+            sleep(1.5)
             viewTasks()
         elif choice == "2":
-            pass
+            addTask()
         elif choice == "3":
             pass
         elif choice == "4":

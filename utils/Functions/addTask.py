56ada7c884b42fed6df66e62be3932debcb231bb
Hydra <hydraplays0001@gmail.com>
2026-01-08 10:50:43 +0530

    Solving a bug?

--- utils/Functions/addTask.py
+++ utils/Functions/addTask.py
@@ -12,7 +12,6 @@ from utils.shared.shared import tasks
 def addTask():
     Task: str = input("Enter Your Task: ")
     sleep(2)
-    dictionary: dict[str, bool] = {Task: False}
     tasks.update(dictionary)
     print("Added Task Successfully! ")
 

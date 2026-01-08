4f8999651a6baf23a755d7c881a438b1f04fee71
Hydra <hydraplays0001@gmail.com>
2026-01-08 10:17:49 +0530

    Adding some comments for Readability

--- utils/Functions/addTask.py
+++ utils/Functions/addTask.py
@@ -1,5 +1,12 @@
+# NOTE: Importing two modules :===
+# NOTE: From time module importing sleep function for some effects.  :)
+# NOTE: From utils.shared.shared importing tasks global variable which a dictionary that stores all the tasks.
 from time import sleep
 from utils.shared.shared import tasks
+
+
+# NOTE: Created a Function named 'addTask' For Adding the tasks in a global variable named tasks.
+# NOTE: which is a dictionary with a key where is your task and a value that tells that is this task done or not in boolean value.
 
 
 def addTask():

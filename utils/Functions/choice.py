dc561b477cc7e0b455d774b522b210b142ee34e1
Hydra <hydraplays0001@gmail.com>
2026-01-11 21:48:14 +0530

    Solving some bugs deletetask.py and choice.py in the utils/Functions/

--- utils/Functions/choice.py
+++ utils/Functions/choice.py
@@ -4,30 +4,38 @@ from utils.Functions.addTask import addTask
 
 
 def userChoice():
-    while True:
-        print(f"{'\t' * 8}***-***-TODO LIST APP-***-***{'\t' * 2}")
-        print("Press:")
-        print("'1' To View The Tasks.")
-        print("'2' To Add a Task.")
-        print("'3' To Delete a Task.")
-        print("'4' To Update The Tasks.")
-        print("'5' To Exit.")
+    try:
+        while True:
+            print(f"{'\t' * 8}***-***-TODO LIST APP-***-***{'\t' * 2}")
+            print("Press:")
+            print("'1' To View The Tasks.")
+            print("'2' To Add a Task.")
+            print("'3' To Delete a Task.")
+            print("'4' To Update The Tasks.")
+            print("'5' To Exit.")
 
-        choice = input("Enter your choice: ")
+            choice = input("Enter your choice: ")
 
-        if choice == "1":
-            viewTasks()
-        elif choice == "2":
-            addTask()
-        elif choice == "3":
-            delete_task()
-        elif choice == "4":
-            pass
-        elif choice == "5" or choice == "exit":
-            print("Thanks For Coming Here. ")
-            break
-        else:
-            print("Invalid choice!")
+            if choice == "1":
+                viewTasks()
+            elif choice == "2":
+                addTask()
+            elif choice == "3":
+                delete_task()
+            elif choice == "4":
+                pass
+            elif choice == "5" or choice == "exit":
+                print("Thanks For Coming Here. ")
+                break
+            else:
+                print("Invalid choice!")
+    except KeyboardInterrupt:
+        print()
+        print("Exited! ")
+    except ValueError:
+        print("Enter A Correct Value! ")
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":

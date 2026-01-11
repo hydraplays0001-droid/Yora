a9d55f4dd07dd8feef7a9595c4b07ad19f74d014
Hydra <hydraplays0001@gmail.com>
2026-01-11 21:54:32 +0530

    Solving some merging bugs in utils/Functions/choice.py

--- utils/Functions/choice.py
+++ utils/Functions/choice.py
@@ -1,26 +1,7 @@
 from utils.Functions.deleteTask import delete_task
 from utils.Functions.viewTask import viewTasks
 from utils.Functions.addTask import addTask
-<<<<<<< HEAD
 from time import sleep
-
-
-def userChoice():
-    print(f"{'\t' * 8}***-***-TODO LIST APP-***-***{'\t' * 2}")
-    print("Press:")
-    print("'1' To View The Tasks.")
-    print("'2' To Add a Task.")
-    print("'3' To Delete a Task.")
-    print("'4' To Update The Tasks.")
-    print("'5' To Exit.")
-
-    while True:
-        try:
-            choice = input("Enter your choice: ")
-
-            if choice == "1":
-                sleep(1.5)
-=======
 
 
 def userChoice():
@@ -37,40 +18,29 @@ def userChoice():
             choice = input("Enter your choice: ")
 
             if choice == "1":
->>>>>>> delete_task
                 viewTasks()
             elif choice == "2":
                 addTask()
             elif choice == "3":
-<<<<<<< HEAD
                 pass
             elif choice == "4":
                 pass
             elif choice == "5":
-=======
                 delete_task()
             elif choice == "4":
                 pass
             elif choice == "5" or choice == "exit":
->>>>>>> delete_task
                 print("Thanks For Coming Here. ")
                 break
             else:
                 print("Invalid choice!")
-<<<<<<< HEAD
-        except KeyboardInterrupt as _:
-            print("")
-            print("Exiting Program!")
-            break
-=======
-    except KeyboardInterrupt:
-        print()
-        print("Exited! ")
+    except KeyboardInterrupt as _:
+        print("")
+        print("Exiting Program!")
     except ValueError:
         print("Enter A Correct Value! ")
     except Exception as e:
         print(e)
->>>>>>> delete_task
 
 
 if __name__ == "__main__":

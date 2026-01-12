c9dcbb80a236ecb97e797c0235405308aea0ba46
Hydra <hydraplays0001@gmail.com>
2026-01-12 18:38:32 +0530

    Created a skeleton model of updateTask function

--- utils/Functions/updateTask.py
+++ utils/Functions/updateTask.py
@@ -2,8 +2,27 @@ from utils.shared.shared import tasks
 
 
 def updateTask():
+    print("Press -> ")
+    print("1 -> To update The context a task. ")
+    print("2 -> To update The Done or Not Done of a task or (True or False)")
     for value in tasks:
         print(f"{value} -> {tasks[value]}")
+    while True:
+        try:
+            choice = input("Enter your choice: ")
+            if choice == "1":
+                pass
+            elif choice == "2":
+                pass
+            else:
+                print("")
+                print("Invalid Choice! Please Enter 1 or 2.")
+        except KeyboardInterrupt:
+            print()
+            print("Exited! ")
+            break
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":

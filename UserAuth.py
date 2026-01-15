1bffcabd7d62db6c781e4f4ce74029a6d7c67d63
Hydra <hydraplays0001@gmail.com>
2026-01-15 11:23:59 +0530

    Solving a bug

--- UserAuth.py
+++ UserAuth.py
@@ -1,5 +1,10 @@
 print("Want To create an account? Press Y to Create The account. ")
 print("Press Y For Existing account That You have. ")
 choice = input("Enter Your Choice -- (Y)/(N): ").lower().strip()
-if choice == "Y":
-    account = input("Enter Your Existing Account: ")
+while True:
+    if choice == "Y":
+        account = input("Enter Your Existing Account: ")
+    elif choice == "N":
+        break
+    else:
+        print("Please enter only Y or N! ")

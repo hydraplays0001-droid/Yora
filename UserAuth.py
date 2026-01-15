24dfe52a4c189e7214c4ce81e251c3c158e864ca
Hydra <hydraplays0001@gmail.com>
2026-01-15 11:32:32 +0530

    Solved the bugs now working on existing account storage system

--- UserAuth.py
+++ UserAuth.py
@@ -1,10 +1,18 @@
-print("Want To create an account? Press Y to Create The account. ")
-print("Press Y For Existing account That You have. ")
-choice = input("Enter Your Choice -- (Y)/(N): ").lower().strip()
-while True:
-    if choice == "Y":
-        account = input("Enter Your Existing Account: ")
-    elif choice == "N":
-        break
-    else:
-        print("Please enter only Y or N! ")
+print("Want to create an account? Press Y to create the account.")
+print("Press N if you already have an existing account.")
+
+try:
+    while True:
+        choice = input("Enter your choice (Y/N): ").lower().strip()
+
+        if choice == "y":
+            break
+        elif choice == "n":
+            account = input("Enter your existing account: ")
+            break
+        else:
+            print("Please enter only Y or N!")
+except Exception as e:
+    print(e)
+except KeyboardInterrupt:
+    print("\nExited")

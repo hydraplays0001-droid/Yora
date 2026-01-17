9f794fcae5c5b23c3a8b5e3da4f16fddb3acd153
Hydra <hydraplays0001@gmail.com>
2026-01-17 08:10:48 +0530

    Solving some bugs and creating the auth of username storage system in only a file

--- UserAuth.py
+++ UserAuth.py
@@ -1,3 +1,5 @@
+import re
+
 print("Want to create an account? Press Y to create the account.")
 print("Press N if you already have an existing account.")
 
@@ -6,10 +8,19 @@ try:
         choice = input("Enter your choice (Y/N): ").lower().strip()
 
         if choice == "y":
+            print(
+                "Continue To With Google 🇬, Apple 🍎, Microsoft 🪟, Facebook 𝐟 , Steam 💨, Twitter 𝕏"
+            )
             break
         elif choice == "n":
-            account = input("Enter your existing account: ")
-            break
+            account = input("Enter Your Existing Account: ")
+            pattern = (
+                r"^[A-Za-z0-9_%#]+(\.[A-Za-z0-9_%#]+)?+@[A-Za-z0-9_%#]+\.(edu|com)$"
+            )
+            if re.fullmatch(pattern, account):
+                print("Valid")
+            else:
+                print("Invalid! ")
         else:
             print("Please enter only Y or N!")
 except Exception as e:
